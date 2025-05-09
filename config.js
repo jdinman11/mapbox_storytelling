76b53ad13638be82a845e07ff3fca7f7735c6241
jdinman11 <dinmanjd@dukes.jmu.edu>
2025-05-09 17:47:41 -0400

    Images

--- config.js
+++ config.js
@@ -54,8 +54,8 @@ var config = {
             id: 'thesis',
             alignment: 'centered',
             hidden: false,
-            title: 'Afrian-African Movement and Purchases',
-            image: 'Assets/hurston.jpeg',
+            title: 'Black Movement and Purchases',
+            image: './assets/south.jpeg',
             description: 'In this essay, I will be examining three novels from three different novels from different points in American history to investigate how these authors depict African-American movement within these novels, and compare that to the picture of African-American life painted by local color stories. Specifically, I will be looking at a possible relationship between Black economic progress, measured by purchases within the novel, and movement. These novels are <em>Their Eyes Were Watching God</em> by Zora Neale Hurston, <em>The Color Purple</em> by Alice Walker, and <em>Salvage The Bones</em> by Jesmyn Ward. ',
             location: {
                 center: [-90.05579,35.15143],
@@ -125,7 +125,7 @@ var config = {
             alignment: 'centered',
             hidden: false,
             title: 'The Color Purple',
-            image: './assets/color_purple_cover.jpg',
+            image: './assets/color_purple.jpeg',
             description: 'In Alice Walker’s novel <em>the Color Purple</em>, we follow the similar journey of Celie. Celie navigates the tumultuous life of a Black woman in the south in the early 20th century. She starts writing to God after her step father impregnates her twice when she is a teenager, and Celie is forced to give up her children. She is married off to a man who routinely abuses her, partially because he is in love with another woman named Shug Avery. Celie eventually meets Shug Avery and they start a relationship. <em>the Color Purple</em> is a novel focusing on Celie’s sexual and spiritual explorations as Black woman in the south, discussing the many intersections of sexuality, race, and gender. <em>The Color Purple</em> has the largest range of movement from Black characters out of any of the novels I will be discussing with 57, as Celie receives letters from her sister Nellie who travels to London and Liberia on missionary work.',
             location: {
                 center: [-90.3150946, 38.6605671],
@@ -155,7 +155,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'The Color Purple',
-            image: './assets/hurston.jpeg',
+            image: './assets/color_purple.jpeg',
             description: 'In <em>The Color Purple</em> There are 25 unique instances of Black characters making purchases, as Shug Avery is a successful singer who travels the country performing for audiences. Celie eventually starts sewing pants for Shug, who then wears them on stage which sparks people’s interest in the pants. Eventually, Cellie starts selling pants to make money. “Then Shug want two more pair just like the first. Then everybody in her band want some. Then orders start to come in from everywhere Shug sing. Pretty soon I’m swamp.” p. 212. This narrative is somewhat similar to <em>Their Eyes Were Watching God</em>, as Celie grows up in poverty and gains some economic independence later in life. With this economic independence comes increased movement within the novel.',
             location: {
                 center: [-83.63465,32.83464], 
@@ -194,7 +194,7 @@ var config = {
             alignment: 'fully',
             hidden: false,
             title: 'Salvage The Bones',
-            image: './assets/color_purple_field.jpg',
+            image: './assets/stb.jpeg',
             description: 'Jesmyn Ward’s 2011 novel, <em>Salvage The Bones</em> follows Esch, a Black teenager navigating an unwanted pregnancy. The story follows Esch’s family as they prepare for Hurricane Katrina and the devastation that will follow. Esch’s sister Skeetah owns a Pitbull that he dogfights named China, and the book opens with China giving birth to multiple pups. China’s motherhood in many ways parallels Esch’s, with both navigating the trials and tribulations of motherhood.',
             location: {
                 center: [-89.09330,30.40415], 
@@ -213,7 +213,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'Salvage The Bones',
-            image: './assets/color_purple_field.jpg',
+            image: './assets/stb.jpeg',
             description: 'In  <em>Salvage The Bones</em>, Esch’s family never reaches the same economic freedom that is reached by Janie and Celie. Her father is an alcoholic and does not have a job, and their car is broken down limiting their mobility. This leads them to alternative methods for attempting to escape their economic position. All instances of mobility are either by foot, or having to get their friend Big Henry to drive them places. This means that there are only 27 instances of mobility in the novel. Of these few instances of movement, they only are in a 31 mile range, that between De Lisle, Mississippi and Martins Pine Hill Estates, Mississippi. Likewise, there are only 18 instances of African-Americans making purchases in the novel.',
             location: {
                 center: [-89.09330,30.40415], 
@@ -232,7 +232,7 @@ var config = {
             alignment: 'centered',
             hidden: false,
             title: 'Power and Limitaions',
-            image: './assets/color_purple_cover.jpg',
+            image: './assets/south.jpeg',
             description: 'The narrative created by local color stories, that of stagnant African-Americans entrenched to the landscape, is largely not one shared by later Black authors in Southern literature, specifically the works of Hurston and Walker, who depict extremely mobile Black characters. The mobility of the characters of Hurston and Walker are in part due to the fact that they reach economic stability, allowing them modes of transportation not granted to that of Ward’s characters. Hopefully this data was able to show the correlation between purchases by Black characters, and Black mobility within the novel. This thesis relies heavily on the positive correlation between mobility and economic means. I could think of several reasons why this correlation exists, for example, in <em>Salvage The Bones</em> they can not afford a car which means that their mobility is limited, and in <em>The Color Purple</em>, Celie’s mobility is limited before she becomes economically stable. While this correlation is apparent in the data, this correlation does not necessarily indicate a causal relationship.',
             location: {
                 center: [-90.3150946, 38.6605671],
